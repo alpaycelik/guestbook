1ef65920e4e911eb770dcbab15241f562e1941b2
alpaycelik <alpay.celik@bil.omu.edu.tr>
2017-09-17 14:06:20 +0300

    tarih formatında değişiklik

--- app/Entry.php
+++ app/Entry.php
@@ -3,15 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon;
+
 class Entry extends Model
 {
-
-
-
-
     protected $table = 'entries';
     protected $fillable = ['username', 'email', 'comment'];
-
 
 }
